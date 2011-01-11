0ce11c513398d776186dfaed3e42a748b133ad1b
Leland Husband <leland.husband@gmail.com>
2011-01-11 13:36:58 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abstractalicious::Application.config.secret_token = 'e4cfb1104e1e58df0cd84900d0a6ddf1f38c729cfcaa2f9fb2f05c692b13e413caf6632dd60dbb10d0aaff1beae9a232a32f4d30d9b057888041488cc827448e'
