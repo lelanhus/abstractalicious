7afae979a69e5e57001c9cb1bcab0a56d148531d
Leland Husband <leland.husband@gmail.com>
2011-01-12 14:56:47 -0600

    added length validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,4 +2,6 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   
   validates :title, :authors, :body, :presence => true
+  validates :title, :authors, :length => { :maximum => 255 }
+  validates :body, :length => { :maximum => 1600 }
 end
