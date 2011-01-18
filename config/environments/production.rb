0da82be9d85371f49e3e7a1e377a79f9c4531022
Leland Husband <leland.husband@gmail.com>
2011-01-18 14:08:01 -0600

    added conferences

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Abstractalicious::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.middleware.use Rack::SslEnforcer
+  
 end
---
+++ db/migrate/20110118200621_create_conferences.rb
@@ -0,0 +1,16 @@
+class CreateConferences < ActiveRecord::Migration
+  def self.up
+    create_table :conferences do |t|
+      t.date :start_date
+      t.date :end_date
+      t.datetime :submission_deadline
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :conferences
+  end
+end
